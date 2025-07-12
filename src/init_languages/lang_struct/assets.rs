9422051eb30ef0e7aa0a110ef4fc15a2af986530
indi-c <indigoconnelly@pm.me>
2025-07-12 19:11:26 +0100

    fixed embedding

--- src/init_languages/lang_struct/assets.rs
+++ src/init_languages/lang_struct/assets.rs
@@ -1,5 +1,6 @@
 use rust_embed::RustEmbed;
 use std::fs;
+use paste::paste;
 
 // macro: takes a path to a folder and the language name
 // embeds the files in the templates folder according to the language name
@@ -10,23 +11,13 @@ macro_rules! embed_templates {
     // embed_templates!("rust", "rust");
     // embed_templates!("rust/src", "rust");
     ($path:expr, $lang:expr) => {
-        #[derive(RustEmbed)]
-        #[folder = concat!("templates/", $path)]
-        #[prefix = concat!($lang, "/")]
-        pub struct concat!($lang, "_base");
-        // if there is a subfolder, it will embed the files in that subfolder in a new asset
-        fs::read_dir(concat!("templates/", $lang))
-            .unwrap()
-            .filter_map(Result::ok)
-            .filter(|entry| entry.path().is_dir())
-            .for_each(|entry| {
-                let subfolder = entry.file_name().to_string_lossy();
-                #[derive(RustEmbed)]
-                #[folder = concat!(ROOT_PATH, $lang, "/", subfolder)]
-                #[prefix = concat!($lang, "/", subfolder, "/")]
-                pub struct concat!($lang, "_", subfolder);
-            });
-    };
+        paste! {
+            #[derive(RustEmbed)]
+            #[folder = [<"templates/" $path>]]
+            #[prefix = [<$lang, "/">]]
+            pub struct [<$lang "_assets">];
+        }
+    }
 }
 
 embed_templates!("rust", "rust");
